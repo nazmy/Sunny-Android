ba8f2dc72fed6928bc304edbfd0b65c22a1856aa
Achmad Nazmy <nazmy@netxcel.com.sg>
2014-09-02 23:24:43 +0800

    Create Weather Provide

---
+++ app/src/main/java/com/android/nazmy/sunny/app/data/WeatherProvider.java
@@ -0,0 +1,80 @@
+package com.android.nazmy.sunny.app.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by nazmy on 8/30/14.
+ */
+public class WeatherProvider extends ContentProvider {
+
+    private static final int WEATHER = 100;
+    private static final int WEATHER_WITH_LOCATION = 101;
+    private static final int WEATHER_WITH_LOCATION_AND_DATE = 102;
+    private static final int LOCATION = 300;
+    private static final int LOCATION_ID = 301;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private WeatherDbHelper mOpenHelper;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        String authority = WeatherContract.CONTENT_AUTHORITY;
+
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER, WEATHER);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER +"/*", WEATHER_WITH_LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_WEATHER +"/*/*", WEATHER_WITH_LOCATION_AND_DATE);
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION, LOCATION);
+        matcher.addURI(authority, WeatherContract.PATH_LOCATION + "/#", LOCATION_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new WeatherDbHelper(getContext());
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings2, String s2) {
+        return null;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        int match  = sUriMatcher.match(uri);
+        switch (match) {
+            case WEATHER_WITH_LOCATION_AND_DATE:
+                return WeatherContract.WeatherEntry.CONTENT_ITEM_TYPE.toString();
+            case WEATHER_WITH_LOCATION:
+                return WeatherContract.WeatherEntry.CONTENT_ITEM_TYPE.toString();
+            case WEATHER:
+                return WeatherContract.WeatherEntry.CONTENT_TYPE.toString();
+            case LOCATION_ID:
+                return WeatherContract.LocationEntry.CONTENT_ITEM_TYPE.toString();
+            case LOCATION:
+                return WeatherContract.LocationEntry.CONTENT_TYPE.toString();
+            default:
+                throw new UnsupportedOperationException("No matches URI :" + uri);
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
+}
